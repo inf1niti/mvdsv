a8c160bc800437c814d057994240c15e7b7c67f6
Ad Infinitum <67801374+inf1niti@users.noreply.github.com>
2026-06-10 11:15:10 -0400

    fix pendulum deviation by sending more packets for self_hooks

--- src/sv_send.c
+++ src/sv_send.c
@@ -939,6 +939,9 @@ static int SV_HookStateRecordType(client_t *client, int playernum)
 		if (client->hook_sent_state[playernum] != hook_client->hook_state) {
 			return mvd_hook_record_full;
 		}
+		if (client == hook_client && hook_client->hook_state == mvd_hook_anchored) {
+			return mvd_hook_record_full;
+		}
 		if (hook_client->hook_state == mvd_hook_anchored
 				&& ((client->netchan.outgoing_sequence + playernum) & 7) == 0) {
 			return mvd_hook_record_full;
